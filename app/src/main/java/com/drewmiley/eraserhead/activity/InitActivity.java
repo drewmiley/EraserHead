a894ebd63d6efc38e1d5ec6191f90caca7e4d2dd
Drew Miley <dmiley@scottlogic.co.uk>
2017-02-16 23:23:59 +0000

    Creates activity package

--- app/src/main/java/com/drewmiley/eraserhead/activity/InitActivity.java
+++ app/src/main/java/com/drewmiley/eraserhead/activity/InitActivity.java
@@ -1,4 +1,4 @@
-package com.drewmiley.eraserhead;
+package com.drewmiley.eraserhead.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.drewmiley.eraserhead.R;
 
 public class InitActivity extends AppCompatActivity {
 
